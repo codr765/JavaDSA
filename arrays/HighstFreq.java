0b61a5b3dc91688ceec96b47d3c37eb62b09f21f
codr765 <aryan852814@gmail.com>
2026-08-11 22:51:48 +0530

    arrays ontinued

--- arrays/HighstFreq.java
+++ arrays/HighstFreq.java
@@ -1,13 +1,29 @@
 public class HighstFreq {
-    static int hgstFreq(int[] a) {
-        int freq = 0;
-        int tmp = 0;
 
-        return freq;
+    static int hgstFreq(int[] a) {
+        int count = 1;
+        int maxCount = 1;
+        int result = a[0];
+
+        for (int i = 1; i < a.length; i++) {
+
+            if (a[i] == a[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            if (count > maxCount) {
+                maxCount = count;
+                result = a[i];
+            }
+        }
+
+        return result;
     }
 
     public static void main(String[] args) {
-        int[] nums = { 2, 4, 4, 5, 5, 5, 5, 6, 6, 7, 8, 9 };
+        int[] nums = {2, 4, 4, 5, 5, 5, 5, 6, 6, 7, 8, 9};
 
         System.out.println(hgstFreq(nums));
     }
