f980133842b9c4a0f41e76c18af51a159189d271
codr765 <aryan852814@gmail.com>
2026-08-20 14:12:45 +0530

    Kadane modified

--- arrays/Kadane.java
+++ arrays/Kadane.java
@@ -1,38 +1,39 @@
 public class Kadane {
 
-static void maxSum(int[] nums) {
-    int currSum = nums[0];
-    int maxSum = nums[0];
+    static void maxSum(int[] nums) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
 
-    int start = 0;
-    int end = 0;
-    int tempStart = 0;
+        int start = 0;
+        int end = 0;
+        int tmpStart = 0;
 
-    for (int i = 1; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
+            if (currSum == 0) {
+                tmpStart = i;
+            }
 
-        if (nums[i] > currSum + nums[i]) {
-            currSum = nums[i];
-            tempStart = i;
-        } else {
             currSum += nums[i];
+
+            if (currSum > maxSum) {
+                maxSum = currSum;
+                start = tmpStart;
+                end = i;
+            }
+
+            if (currSum < 0) {
+                currSum = 0;
+            }
         }
 
-        if (currSum > maxSum) {
-            maxSum = currSum;
-            start = tempStart;
-            end = i;
-        }
+        System.out.println("Max Sum: " + maxSum);
+        System.out.println("Start Index: " + start);
+        System.out.println("End Index: " + end);
     }
 
-    System.out.println("Max Sum: " + maxSum);
-    System.out.println("Start Index: " + start);
-    System.out.println("End Index: " + end);
-}
+    public static void main(String[] args) {
+        int[] nums = { 1, 5, -3, 7, -4, -2, -4, 2 };
 
-
-	public static void main(String[] args) {
-		int[] nums = { 1, 5, -3, 7, -4, -2, -4, 2 };
-		
-		maxSum(nums);
-	}
+        maxSum(nums);
+    }
 }
