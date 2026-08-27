89ad2dad3f3f40a02cd01ba79b4501f9569c9854
codr765 <aryan852814@gmail.com>
2026-08-27 23:25:10 +0530

    nums

---
+++ numbers/DecToBin.java
@@ -0,0 +1,21 @@
+public class DecToBin {
+    static int decToBin(int dec) {
+        int bin = 0;
+        int n = 1;
+
+        while (dec > 0) {
+            int digit = dec % 2;
+            bin = bin + n * digit;
+            dec /= 2;
+            n *= 10;
+        }
+
+        return bin;
+    }
+
+    public static void main(String[] args) {
+        int dec = 14;
+
+        System.out.println(decToBin(dec));
+    }
+}
