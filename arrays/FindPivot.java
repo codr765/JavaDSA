07df974a5cb82051cfa55af870d5c86c01a7a05b
codr765 <aryan852814@gmail.com>
2026-09-04 18:59:20 +0530

    find pivot

--- arrays/FindPivot.java
+++ arrays/FindPivot.java
@@ -2,8 +2,6 @@ public class FindPivot {
     public static void main(String[] args) {
         int[] nums = { 4, 5, 6, 7, 8, 9, 0, 1, 2, 3 };
 
-        // 0,1,2,3,4,5,6,7,8,9
-
         int curr = nums[0];
 
         for (int i = 1; i < nums.length; i++) {
---
+++ arrays/MoveZeroesFront.java
@@ -0,0 +1,8 @@
+public class MoveZeroesFront {
+    public static void main(String[] args) {
+        int[] nums = { 1, 0, 20, 4, 3, 0, 0, 5 };
+
+        // {0,0,0,1,20,4,3,5}
+        
+    }
+}
