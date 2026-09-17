c3f60de2bd8dd58c5309358bdb1d6bd1ede30102
codr765 <aryan852814@gmail.com>
2026-09-17 21:21:01 +0530

    dups2

---
+++ arrays/Dups2.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+
+public class Dups2 {
+
+    static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+
+            set.add(num);
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5, 1, 2 };
+
+        System.out.println(containsDuplicate(nums));
+    }
+}
